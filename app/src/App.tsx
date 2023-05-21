8ccfb2242638fae4ecc07604ef24672a54f65346
arjun <arjun232001@gmail.com>
2023-05-21 08:44:59 +0530

    fixing config

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,18 @@
 import axios from 'axios';
 import './App.css';
+import {useState} from "react";
+
 
 function App() {
+    const[data, setData] = useState(null);
 
     axios.get("http://localhost:5000/").then(res => {
-            console.log(res);
+
+                setData(res.data.message);
             })
   return (
-    <div className="App"> 
+    <div className="App">
+        {data}
     </div>
   );
 }
